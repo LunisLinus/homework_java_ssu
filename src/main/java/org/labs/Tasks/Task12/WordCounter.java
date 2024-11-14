108bbe8e41da301eab5e01d98faacf99d61a0793
Sonya <142392876+LunisLinus@users.noreply.github.com>
2024-11-14 09:02:23 +0400

    Лабораторная работа 7 Задание 12

---
+++ src/main/java/org/labs/Tasks/Task12/WordCounter.java
@@ -0,0 +1,22 @@
+package org.labs.Tasks.Task12;
+
+import java.io.IOException;
+import java.nio.file.*;
+
+
+public class WordCounter {
+
+    public static void main(String[] args) {
+        Path startingDir = Paths.get("src/main/resources");
+
+        WordCountingFileVisitor visitor = new WordCountingFileVisitor();
+
+        try {
+            Files.walkFileTree(startingDir, visitor);
+            System.out.println("Общее количество слов во всех текстовых файлах: " + visitor.getTotalWordCount());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
