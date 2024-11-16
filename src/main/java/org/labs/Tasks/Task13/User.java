1a9c99231769d6329355f1670e81cdd2cf98b043
Sonya <142392876+LunisLinus@users.noreply.github.com>
2024-11-16 10:44:05 +0400

    Лабораторная работа № 7 Задание 13

---
+++ src/main/java/org/labs/Tasks/Task13/User.java
@@ -0,0 +1,34 @@
+package org.labs.Tasks.Task13;
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private final String name;
+    private final int age;
+    private final String email;
+
+    public User(String name, int age, String email) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "Имя: " + name + ", Возраст: " + age + ", Email: " + email;
+    }
+}
+
