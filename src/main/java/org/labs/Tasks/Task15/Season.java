4c0f1ef66b12ada5b611b0c807c511fd0a99f218
Sonya <142392876+LunisLinus@users.noreply.github.com>
2024-11-23 08:59:54 +0400

    Лабораторная работа № 9 Задание 15

---
+++ src/main/java/org/labs/Tasks/Task15/Season.java
@@ -0,0 +1,24 @@
+package org.labs.Tasks.Task15;
+
+public enum Season {
+    SPRING("Тепло", "Пасха"),
+    SUMMER("Жарко", "День Независимости"),
+    AUTUMN("Прохладно", "День учителя"),
+    WINTER("Холодно", "Новый год");
+
+    private final String temperature;
+    private final String typicalHoliday;
+
+    Season(String temperature, String typicalHoliday) {
+        this.temperature = temperature;
+        this.typicalHoliday = typicalHoliday;
+    }
+
+    public String getTemperature() {
+        return temperature;
+    }
+
+    public String getTypicalHoliday() {
+        return typicalHoliday;
+    }
+}
