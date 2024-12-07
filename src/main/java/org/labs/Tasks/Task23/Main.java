3cc6d2b3a000d025761223298b8483b25dbc71f1
Sonya <142392876+LunisLinus@users.noreply.github.com>
2024-12-07 07:33:05 +0400

    Лабораторная работа № 10 Задание 23

---
+++ src/main/java/org/labs/Tasks/Task23/Main.java
@@ -0,0 +1,31 @@
+package org.labs.Tasks.Task23;
+
+// Main.java
+
+public class Main {
+    public static void main(String[] args) {
+        Integer number = 123;
+        Utils.printType(number);
+
+        String text = "Hello";
+        Utils.printType(text);
+
+        Double decimal = 45.67;
+        Utils.printType(decimal);
+
+        Boolean flag = true;
+        Utils.printType(flag);
+
+        Person person = new Person("Alice", 30);
+        Utils.printType(person);
+
+        Utils.printType(null);
+
+        int[] array = {1, 2, 3};
+        Utils.printType(array);
+
+        java.util.List<String> list = java.util.Arrays.asList("apple", "banana");
+        Utils.printType(list);
+    }
+}
+
