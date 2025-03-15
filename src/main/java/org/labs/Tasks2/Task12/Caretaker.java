d3ad2c8c43b695c30b054e7462e3334eb20801a0
Sonya <142392876+LunisLinus@users.noreply.github.com>
2025-03-15 08:44:59 +0400

    Лабораторная работа № 2.4 Задание 12

---
+++ src/main/java/org/labs/Tasks2/Task12/Caretaker.java
@@ -0,0 +1,25 @@
+package org.labs.Tasks2.Task12;
+
+import java.util.Stack;
+
+class Caretaker {
+    private final Stack<Memento> history = new Stack<>();
+
+    public void saveState(TextEditor editor) {
+        history.push(editor.saveState());
+    }
+
+    public void undo(TextEditor editor) {
+        if (!history.isEmpty()) {
+            Memento memento = history.pop();
+            editor.restoreState(memento);
+        } else {
+            System.out.println("Нет сохранённых состояний для отката.");
+        }
+    }
+
+    public boolean canUndo() {
+        return !history.isEmpty();
+    }
+}
+
